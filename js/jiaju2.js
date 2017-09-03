051db7b8c174432df8bb069d7084eb148fcf4705
kidceg <31203059+kidceg@users.noreply.github.com>
2017-09-03 21:02:08 +0800

    add sign up jQuery

--- js/jiaju2.js
+++ js/jiaju2.js
@@ -37,157 +37,183 @@ var $passtips = $('#passtips');
 
 
 //首页右上角注册按钮点击,弹出注册框，背景改变
-$('#sign').on('click',function(){
-	$sign_upc_ontainer.css('display','block');
+$('#sign').on('click', function () {
+	$sign_upc_ontainer.css('display', 'block');
 });
 
 //注册框关闭按钮点击
-$signUpClose.on('click',function(){
-	$sign_upc_ontainer.css('display','none');
+$signUpClose.on('click', function () {
+	$sign_upc_ontainer.css('display', 'none');
 	$tips.html('　');
-	$passtips.css('display','none');
-	$text.css('border','').val('');
+	$passtips.css('display', 'none');
+	$text.css('border', '')
+  .val('');
 });
 
 //各种验证正则匹配
+//字母数字下划线字符数4-15个字符
 var pUser = /^(\w{4,15})$/;
+//邮件格式
 var pEmail =/^([\w\.\-]+)@([\w\.\-]+)\.([\w]{2,4})$/;
+//任意8-16个字符
 var pPass1=/^(.{8,16})$/;
 
 
+//声明用来赋值最后判断注册条件是否成立
+var checkUser = 0;
+var checkEmail = 0;
+var checkPassword = 0;
+var checkPassword2 = 0;
 
 //用户名的验证，输入框失去焦点后判断是否符合要求
-$user.on('blur', function(){
-    if ($user.val().length!==0 
-    	&& pUser.test($user.val())==false){
+$user.on('blur', function () {
+    if ($user.val().length !== 0 
+    	&& pUser.test($user.val()) == false) {
 	    $tips.html('错误提示：用户名4-15字符，可以是大小写字母、数字、下划线。');
-	    $user.css('border','1px solid red');
+	    $user.css('border', '1px solid red');
 	    $signButton.css({
 	    	'color':'',
 		    'background-color':''
 		});
-	}else{
+		checkUser = 0;
+	} else {  
 	    $tips.html('　');
 	    $user.css('border','');
-	     
+	    checkUser = 0;	     
     }
-    if (pUser.test($user.value)==true){
-     	return checkNum===1;
+    if (pUser.test($user.val()) == true) {
+     	checkUser = 1;
     }
 
-})
+});
 
 //邮件的验证，输入框失去焦点后判断是否符合要求
-$email.on('blur',function(){
-    if ($email.val().length!==0 
-    	&& pEmail.test($email.val())==false){
+$email.on('blur',  function () {
+    if ($email.val().length !== 0 
+    	&& pEmail.test($email.val()) == false) {
 	    $tips.html('错误提示：请输入正确的邮箱');
 	    $email.css('border','1px solid red');
 	    $signButton.css({
 	    	'color':'',
 	    	'background-color':''
 	    });
-    }else{
+	    checkEmail = 0;
+    } else {
 	    $tips.html('　');
 	    $email.css('border','');
+	    checkEmail = 0;
     }
-    if (pEmail.test($email.val())==true){return checkNum +=1;}
-})
+    if (pEmail.test($email.val()) == true) {
+    	checkEmail = 1;
+    }
+});
 
-//声明checkNum用来赋值最后判断注册条件是否成立
-var checkNum = 0;
+
 //密码的验证，输入框失去焦点后判断是否符合要求
-$password.on('blur',function(){
-	//符合条件就checkSum加1，用来综合判断密码强度
+$password.on('blur', function () {
+	//checkSum符合条件就加1，用来综合判断密码强度
     var checkSum = 0;
-    if (/\d/g.test($password.val())==true){
+    //判断是否含有数字
+    if (/\d/g.test($password.val()) == true) {
     	checkSum = 1;
     };
-    if (/[a-z]/ig.test($password.val())==true){
+    //判断是否含有字母（含大小写）
+    if (/[a-z]/ig.test($password.val()) == true) {
     	checkSum +=1;
     };
-    if (/[^0-9a-zA-Z]/g.test($password.val())==true){
+    //判断是否含有除了字母数字外的其他字符
+    if (/[^0-9a-zA-Z]/g.test($password.val()) == true) {
     	checkSum += 1;
     };
 
-    if ((checkSum==3
+    if ((checkSum == 3
     	  && $password.val().length>7 
     	  && $password.val().length<17) 
-    	|| (checkSum==2
+    	|| (checkSum == 2
     		&& $password.val().length>12 
-    		&& $password.val().length<17)){     
+    		&& $password.val().length<17)) {     
         $tips.html('　');
         $password.css('border','');
-	    $passtips.html('强').css({
-	    	'display':'block',
-	    	'background-color':'#48C157',
-	    	'width':'66px',
-	        });
-        return checkNum +=1;
-    }else if ((checkSum==2 
+  	    $passtips.html('强')
+        .css({
+  	    	'display':'block',
+  	    	'background-color':'#48C157',
+  	    	'width':'66px',
+  	        });
+        checkPassword = 1;
+    } else if ((checkSum == 2 
 		    	&& $password.val().length>7 
 		    	&& $password.val().length<13) 
-		     || (checkSum==1
+		     || (checkSum == 1
 		    	&& $password.val().length>14 
-		    	&& $password.val().length<17)){
+		    	&& $password.val().length<17)) {
     	$tips.html('　');
 	    $password.css('border','');
-	    $passtips.html('中').css({
+	    $passtips.html('中')
+      .css({
 	    	'display':'block',
 	    	'background-color':'#F68324',
 	    	'width':'44px'
 		    });
-	    return checkNum +=1;
-    }else if (checkSum==1 
+	    checkPassword = 1;
+    }else if (checkSum == 1 
     	      && $password.val().length>7 
-    	      && $password.val().length<15){
+    	      && $password.val().length<15) {
 	    $tips.html('　');
 	    $password.css('border','');
-	    $passtips.html('弱').css({
+	    $passtips.html('弱')
+      .css({
 	      	'display':'block',
 	      	'background-color':'red',
 	      	'width':'22px'
 	      });     
-	    return checkNum +=1;
-    }else if ($password.val().length==0){
+	    checkPassword = 1;
+    } else if ($password.val().length == 0) {
       $tips.html('　');
       $password.css('border','');
       $passtips.css('display','none');
-    }else{
+      checkPassword = 0;
+    } else {
       $passtips.css('display','none');
       $tips.html('错误提示：密码字符长度8-16。');
       $password.css('border','1px solid red');
       $signButton.css({
       	'color':'',
       	'background-color':''
-      })
+      });
+      checkPassword = 0;
 
     }
 
-})
+});
 
 //确认密码是否一致
-$password2.on('blur',function(){
-  if ($password2.val() !== $password.val()){
+$password2.on('blur', function () {
+  if ($password2.val() !== $password.val()) {
     $tips.html('错误提示：密码不一致');
     $password2.css('border', '1px solid red');
     $signButton.css({
     	'color':'',
     	'background-color':''
     });
-  }else{
+    checkPassword2 = 0;
+  } else {
     $tips.html('　');
     $password2.css('border', '');
-    return checkNum+=1;
+    checkPassword2 = 1;
   }
 
-})
+});
 
 
 //点击注册按钮判断是否完善
-$signButton.on('click',function(){
- if (checkNum===4){
-    $tips.html('注册成功，将在3s内关闭本窗口。').css('color','#48C157');
+$signButton.on('click', function () {
+ if (checkUser == 1 
+ 	&& checkEmail == 1
+ 	&& checkPassword == 1
+ 	&& checkPassword2 == 1) {
+    $tips.html('注册成功，将在3s内关闭本窗口。')
+    .css('color','#48C157');
     $signButton.css({
     	'color':'white',
     	'background-color':'#48C157'
@@ -195,24 +221,26 @@ $signButton.on('click',function(){
     var num = 4;                
     var min = 0;  
     //倒计时
-    var countsDown = setInterval(function () {  
+    var countsDown = setInterval(function () {
         num--;    
-       $tips.html('注册成功，将在3s内关闭本窗口:　'+num+'s');               
-		if (num == min){           
-		    clearInterval(counstDown);          
-		    $sign_upc_ontainer.css('display','none'); 
-		    $signButton.css({
-		    	'background-color':'',
-		    	'color':''
-		    })
-		    $tips.html('　').css('color','');
-		    $passtips.css('display','none');
-		    $text.val('').css('border','');
-		}
-	   
-	}, 1000);  
- }
-})
+       $tips.html('注册成功，将在3s内关闭本窗口:　'+num+'s');
+       if (num == min) {           
+  		    clearInterval(countsDown);   
+  		    //倒计时结束后的操作       
+  		    $sign_upc_ontainer.css('display','none'); 
+  		    $signButton.css({
+  		    	'background-color':'',
+  		    	'color':''
+  		    })
+  		    $tips.html('　')
+          .css('color','');
+  		    $passtips.css('display','none');
+  		    $text.val('')
+          .css('border','');
+  		 }	   
+	   }, 1000);  
+   }
+});
 
 
 
